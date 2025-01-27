12874187cca7d6d49425875a73f333f6c85aab5f
Lasergtx <131684618+Lasergtx@users.noreply.github.com>
2025-01-27 17:22:45 +0100

    feat: data

--- ElearningApi/index.js
+++ ElearningApi/index.js
@@ -1,16 +1,88 @@
 const express = require("express");
 const app = express();
-const coursesRoute = require("./routes/courses");
-const lessonsRoute = require("./routes/lessons");
-const lessonsCompletedRoute = require("./routes/lessonsCompleted");
-const loginRoute = require("./routes/login");
+const fs = require("fs");
+const path = require("path");
+// const coursesRoute = require("./routes/courses");
+// const lessonsRoute = require("./routes/lessons");
+// const lessonsCompletedRoute = require("./routes/lessonsCompleted");
+// const loginRoute = require("./routes/login");
+
+const dataPath = path.join(__dirname, "data.json");
+let data = {};
+
+fs.readFile(dataPath, "utf8", (err, fileData) => {
+    if (err) {
+        console.error("Error reading JSON file", err);
+        return;
+    }
+    data = JSON.parse(fileData);
+});
 
 app.use(express.json());
 
-app.use("/api", coursesRoute);
-app.use("/api", lessonsRoute);
-app.use("/api", lessonsCompletedRoute);
-app.use("/api", loginRoute);
+app.get("/courses", (req, res) => {
+    if (req.query.id) {
+        res.json(
+            data.courses.find((course) => course.id === parseInt(req.query.id))
+        );
+    } else {
+        res.json(data.courses);
+    }
+});
+
+app.get("/lessons", (req, res) => {
+    if (req.query.courseId) {
+        res.json(
+            data.lessons.filter(
+                (lesson) => lesson.course_id === parseInt(req.query.courseId)
+            )
+        );
+    } else {
+        res.json("Auccun id de cours spécifié");
+    }
+});
+
+app.get("/lessons-completed", (req, res) => {
+    if (req.query.courseId && req.query.userId && req.query.status) {
+        res.json(
+            data.progress.filter(
+                (lessonCompleted) =>
+                    lessonCompleted.course_id ===
+                        parseInt(req.query.courseId) &&
+                    lessonCompleted.user_id === parseInt(req.query.userId) &&
+                    lessonCompleted.status === req.query.status
+            )
+        );
+    } else {
+        res.json(
+            !req.query.courseId
+                ? "Aucun id de cours spécifié"
+                : !req.query.userId
+                ? "Aucun id d'utilisateur spécifié"
+                : "Aucun statut spécifié"
+        );
+    }
+});
+
+app.get("/login", (req, res) => {
+    if (req.query.email && req.query.password) {
+        data.users.forEach((user) => {
+            if (
+                user.email === req.query.email &&
+                user.password === req.query.password
+            ) {
+                res.json(true);
+            }
+        });
+        res.json(false);
+    } else {
+        res.json("Veuillez saisir votre email et votre mot de passe");
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # elearningG4
+
+Bonjour,
+
+Bienvenue sur l'api d'e-learning G4. Une fois votre serveur lancer vous pourrez récupérer les données dont vous avez besoin via les routes suivantes :
+
+-   /courses Permet de récupérer une liste de tout les cours ainsi que leurs données associées.
+-   /courses?id=:id Permet de récupérer les informations d'un cours en particulier via son id.
+-   /lessons?courseId=:id Permet de récupérer une liste des leçons ainsi que leurs données via l'id du cours associé à celles-ci.
+-   /lessons-completed?courseId=:id&userId=:id&status=status Permet de récupérer une liste associée à un utilisateur et un cours celons les id et le statut entrer en paramètres.
+-   /login?email=email&password=password Permet de vérifier l'existence d'un utilisateur en bdd en renvoyant true ou false.
